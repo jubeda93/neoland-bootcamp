5ed0890e9f95353a5939e97ada9e39f64d265e5f
jubeda <jubeda93@gmail.com>
2025-03-20 18:49:42 +0100

    docs: :memo: Recopilacion datos, bucle y fichas pokemons

---
+++ src/js/funciones.js
@@ -0,0 +1,16 @@
+// Estricturta de una funcion
+
+/**
+ * Genera un string con camelCase a partir de un texto y un numero
+ * @param {string} textoSaludo - El texto a convertir
+ * @param {number} [numeroAsiento=1] - El numero a concatenar
+ * @returns {string}
+ */
+function nombreEnCamelCase(textoSaludo, numeroAsiento = 1) {
+
+    console.log(textoSaludo, numeroAsiento)
+
+    return 'mensaje recibido'
+}
+
+nombreEnCamelCase('hola', 27)
--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -2,7 +2,65 @@ import pokedex from '../pokedex/pokedex.json' with { type: "json" }
 console.log('Ya se ha cargado completamente')
 window.addEventListener("DOMContentLoaded", onDOMContentLoaded)
 
-/* Tarea: buscar un pokemon
+
+
+function onDOMContentLoaded() {
+    let listaPokemon = document.getElementsByClassName('lista-pokemon')[0]
+    console.log('Ya esta disponible la pagina', listaPokemon);
+
+    for (let i = 0; i < 12; i++) {
+        let liElement = document.createElement('li');
+        console.log(pokedex[i].name.english);
+        // nuevoPokemon.innerText = pokedex[i].name.english;
+
+        let figureElement = document.createElement('figure');
+        figureElement.classList.add('pokemon');
+
+
+        //  //Creo el elementro para cargar la imagen:
+        let imgElement = document.createElement('img')
+        let idPokemon = String(pokedex[i].id);
+        imgElement.src = '/pokedex/images/' + idPokemon.padStart(3, '0') + '.png'
+        figureElement.appendChild(imgElement);
+
+        //Creo el elementro para el id/numero:
+        let figcaptionElement = document.createElement('figcaption');
+        figcaptionElement.innerText = 'Nº. ' + idPokemon.padStart(4, '0')
+        figcaptionElement.classList.add('numeropokemon');
+        figureElement.appendChild(figcaptionElement);
+
+        //Creo el elementro para el Nombre:
+        let pElement = document.createElement('p');
+        pElement.innerText = pokedex[i].name.english;
+        pElement.classList.add('nombrepokemon');
+        figureElement.appendChild(pElement);
+
+
+        //Creo el elementro para el tipo:
+        let pClasesElement = document.createElement('p');
+        pClasesElement.classList.add('clases');
+        
+        //por cada tipo de pokemon, generamos un span
+
+        for (let j = 0; j < pokedex[i].type.length; j++) {
+            let spanElement = document.createElement('span');
+            spanElement.innerText = pokedex[i].type[j];
+            spanElement.classList.add('tag');
+            spanElement.classList.add(pokedex[i].type[j].toLowerCase());
+            pClasesElement.appendChild(spanElement);
+        }
+
+
+
+        figureElement.appendChild(pClasesElement)
+
+        liElement.appendChild(figureElement)
+
+        listaPokemon.appendChild(liElement);
+
+    }
+
+    /* Tarea: buscar un pokemon
 
 1.Escribo en el formulario el nombre del pokemon  
 2.pincho en boton de buscar
@@ -13,60 +71,30 @@ window.addEventListener("DOMContentLoaded", onDOMContentLoaded)
 */
 
 
-/*
-1. INPUT del nombre o numero del pokemon
-2. SET la info del pokemon que se quiere buscar
-3. READ los archivos de la base de datos
-4. If-else, en caso de encontrar los datos solicitados muestra estos
-5. SHOW, filtro el pokemon que buscamos, lo muestramos en pagina principal y HIDE el resto de pokemons
-6. Display con dicha pagina principal donde solo mostramos el pokemon que buscamos
-
-
-
- 
-INPUT: pides información al usuario
-SET: guardas información en una variable
-PRINT: muestras un valor o mensaje en la pantalla
-READ: lees un archivo o documento
-DISPLAY: muestras una ventana nueva
-SHOW / HIDE: muestras u ocultas un elemento de interfaz
-CALCULATE: realiza un cálculo
-IF-ELSE: ejecuta una acción si se cumple una condición (o en el caso de que no se cumpla)
-CASE: realiza acciones a partir de una lista de preguntas sobre un mismo valor
-FOR: realiza acciones sobre una lista de elementos, o a partir de ellos
-WHILE: realiza acciones mientras se cumpla una condición
-DO-WHILE / REPEAT-UNTIL: realiza una acción, y luego continúa haciéndolo mientras se cumpla una condición (o deje de cumplirse)
-*/
-
-
-
-function onDOMContentLoaded() {
-    let listaPokemon = document.getElementsByClassName('lista-pokemon')[0]
-    console.log('Ya esta disponible la pagina',listaPokemon);
+    /*
+    1. INPUT del nombre o numero del pokemon
+    2. SET la info del pokemon que se quiere buscar
+    3. READ los archivos de la base de datos
+    4. If-else, en caso de encontrar los datos solicitados muestra estos
+    5. SHOW, filtro el pokemon que buscamos, lo muestramos en pagina principal y HIDE el resto de pokemons
+    6. Display con dicha pagina principal donde solo mostramos el pokemon que buscamos
     
-    for (let i = 0; i < 10; i++) { //ToDo: 1.añadir parrafo. 2.añadir InnerText 3.añadir parrafo al elemento de lista
-        let nuevoPokemon = document.createElement('li');
-       // nuevoPokemon.innerText = pokedex[i].name.english;
-        console.log(pokedex[i].name.english);
-       
-
-       let nombrePokemon = document.createElement('p');
-       nombrePokemon.innerText = pokedex[i].name.english;
-       nombrePokemon.classList.add('nombrepokemon');
-       nuevoPokemon.appendChild(nombrePokemon);
-        listaPokemon.appendChild(nuevoPokemon);
-
-        let numeroPokemon = document.createElement('figcaption');
-        numeroPokemon.innerText = pokedex[i].id;
-        numeroPôkemon.classList.add('numeropokemon');
-        nuevoPokemon.appendChild(numeroPokemon);
-
-    }
-
-
-    /* buscarPokemon('Pikachu', 1993, ['fuego', 'agua', 'veneno'])*/
-
-
+    
+    
+     
+    INPUT: pides información al usuario
+    SET: guardas información en una variable
+    PRINT: muestras un valor o mensaje en la pantalla
+    READ: lees un archivo o documento
+    DISPLAY: muestras una ventana nueva
+    SHOW / HIDE: muestras u ocultas un elemento de interfaz
+    CALCULATE: realiza un cálculo
+    IF-ELSE: ejecuta una acción si se cumple una condición (o en el caso de que no se cumpla)
+    CASE: realiza acciones a partir de una lista de preguntas sobre un mismo valor
+    FOR: realiza acciones sobre una lista de elementos, o a partir de ellos
+    WHILE: realiza acciones mientras se cumpla una condición
+    DO-WHILE / REPEAT-UNTIL: realiza una acción, y luego continúa haciéndolo mientras se cumpla una condición (o deje de cumplirse)
+    */
 
 
 
