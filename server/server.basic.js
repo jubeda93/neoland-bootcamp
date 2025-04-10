2fefce0313437222f59aa2b1efbcb3d17826824c
jubeda <jubeda93@gmail.com>
2025-04-10 10:34:22 +0200

    node

---
+++ server/server.basic.js
@@ -0,0 +1,15 @@
+// /server/index.js
+import * as http from "node:http";
+import * as url from "node:url";
+
+http.createServer(function server_onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+
+    console.log("Request for " + pathname + " received.");
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write("<h1>Hello World</h1>");
+    response.end();
+}).listen(process.env.PORT, process.env.IP);
+
+console.log('Server running at http://' + process.env.IP + ':' + process.env.PORT + '/');
